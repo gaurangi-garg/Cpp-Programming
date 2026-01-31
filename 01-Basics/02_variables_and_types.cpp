72c6ee9b3a4d8898569949348fe9fcf47baca64b
gaurangi-garg <gaurangi293@gmail.com>
2026-01-31 19:39:55 +0530

    Create 02_variables_and_types.cpp

---
+++ 01-Basics/02_variables_and_types.cpp
@@ -0,0 +1,10 @@
+// Print Integer
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Please enter an integer:";
+    cin >> n;
+    cout << "You entered "<< n;
+    return 0;
+}
