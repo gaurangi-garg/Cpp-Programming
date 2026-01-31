bf55b00fe2bd4efeabca8eaff1895c988a6d9ae2
gaurangi-garg <gaurangi293@gmail.com>
2026-01-31 20:19:35 +0530

    Create 06_product_of_two_numbers.cpp

---
+++ 01-Basics/06_product_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+// Product of two numbers
+# include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,product;
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+    product = num1 * num2;
+    cout << "Product of " << num1 << " and " << num2 << " is " << product << endl;
+    return 0;
+}
