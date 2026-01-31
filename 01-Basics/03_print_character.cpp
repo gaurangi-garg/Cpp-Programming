bdf443252bda8024414561dc89b48047456d38e6
gaurangi-garg <gaurangi293@gmail.com>
2026-01-31 19:55:30 +0530

    Create 03_print_character.cpp

---
+++ 01-Basics/03_print_character.cpp
@@ -0,0 +1,10 @@
+// Print Character
+# include <iostream>
+using namespace std;
+int main(){
+    char character;
+    cout << "Enter a Character:";
+    cin >> character;
+    cout << "You entered character "<<character;
+    return 0;
+}
