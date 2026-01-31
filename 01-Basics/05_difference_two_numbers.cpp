9a073c3738c1b2e23c365b4a1e1b49f0e1ae0593
gaurangi-garg <gaurangi293@gmail.com>
2026-01-31 20:11:53 +0530

    Create 05_difference_two_numbers.cpp

---
+++ 01-Basics/05_difference_two_numbers.cpp
@@ -0,0 +1,13 @@
+// Difference of two numbers
+# include <iostream>
+using namespace std;
+int main(){
+    int num1,num2,difference;
+    cout << "Enter first number:";
+    cin >> num1;
+    cout << "Enter second number:";
+    cin >> num2;
+    difference=num1-num2;
+    cout << "Difference of two numbers is " << difference;
+    return 0;
+}
