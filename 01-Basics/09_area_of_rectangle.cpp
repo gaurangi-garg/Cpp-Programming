25d78a957ec51ba880aa7c673205448459a11226
gaurangi-garg <gaurangi293@gmail.com>
2026-02-02 12:50:49 +0530

    Create 09_area_of_rectangle.cpp

---
+++ 01-Basics/09_area_of_rectangle.cpp
@@ -0,0 +1,13 @@
+// area of rectangle
+# include <iostream>
+using namespace std;
+int main(){
+    float length,breadth,area;
+    cout << "Enter the length of rectangle: " << endl;
+    cin >> length;
+    cout << "Enter the breadth of rectangle: " << endl;
+    cin >> breadth;
+    area=length*breadth;
+    cout << "Area of rectangle is " << area << " square units";
+    return 0;
+}
